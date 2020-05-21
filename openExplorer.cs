42de8d0cde29ed756bacc08c0716fdb26ad273c3
git-oaktree <git-oaktree@users.noreply.github.com>
2020-05-21 14:43:25 -0400

    Create openExplorer.cs

---
+++ openExplorer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+
+namespace open
+{
+    class Program
+    {
+
+        private void StartProcess(string path)
+        {
+            ProcessStartInfo StartInformation = new ProcessStartInfo();
+
+            StartInformation.FileName = path;
+
+            Process process = Process.Start(StartInformation);
+
+            process.EnableRaisingEvents = true;
+        }
+
+        static void Main(string[] args)
+        {
+            string folderPath = Directory.GetCurrentDirectory();
+            string dumpFile = String.Format("Current Directory is {0}", folderPath);
+            Console.WriteLine(dumpFile);
+            Process.Start("explorer.exe", folderPath);
+            //Program test = new Program();
+            //test.StartProcess(folderPath);
+        }
+    }
+}
